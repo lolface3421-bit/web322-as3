3a584093491fe50ade41c25a39b4956c83ca5eef
Jimmy Yang <jyang227@myseneca.ca>
2025-12-02 22:27:16 -0500

    Initial commit for WEB322 AS3

--- modules/projects.js
+++ modules/projects.js
@@ -1,67 +1,138 @@
-const projectData = require("../data/projectData")
-const sectorData = require("../data/sectorData")
+require('dotenv').config()
 
-let projects = []
+require('pg')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+    }
+  }
+)
+
+const Sector = sequelize.define('Sector', {
+  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  sector_name: Sequelize.STRING
+}, {createdAt: false, updatedAt: false})
+
+const Project = sequelize.define('Project', {
+  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  title: Sequelize.STRING,
+  feature_img_url: Sequelize.STRING,
+  summary_short: Sequelize.TEXT,
+  intro_short: Sequelize.TEXT,
+  impact: Sequelize.TEXT,
+  original_source_url: Sequelize.STRING
+}, {createdAt: false, updatedAt: false})
+
+Project.belongsTo(Sector, { foreignKey: 'sector_id' })
 
 function Initialize() {
   return new Promise((resolve, reject) => {
-    try {
-      projects = []
-      projectData.forEach((proj) => {
-        const sectorObj = sectorData.find((sec) => sec.id === proj.sector_id)
-
-        const copy = {
-          ...proj,
-          sector: sectorObj ? sectorObj.sector_name : null,
-        }
-        projects.push(copy)
-      })
-      resolve()
-    } catch (error) {
-      reject("Failed to init projects")
-    }
-  })
+    sequelize.sync()
+      .then(() => resolve())
+      .catch((err) => reject(err))
+  });
 }
 
 function getAllProjects() {
-  return new Promise((resolve, reject) => {
-    if (projects.length > 0) {
-      resolve(projects)
-    } else {
-      reject("No projects were found")
-    }
-  })
+    return new Promise((resolve, reject) => {
+    Project.findAll({ include: [Sector] })
+      .then((projects) => resolve(projects))
+      .catch((err) => reject("No projects were found"))
+  });
 }
 
 function getProjectById(projectId) {
   return new Promise((resolve, reject) => {
-    const project = projects.find((p) => p.id === projectId)
-    if (project) {
-      resolve(project)
-    } else {
-      reject(`Unable to find project with id: ${projectId}`)
-    }
-  })
+    Project.findAll({ 
+      include: [Sector],
+      where: { id: projectId }
+    })
+      .then((projects) => {
+        if (projects.length > 0) resolve(projects[0]);
+        else reject(`Unable to find project with id: ${projectId}`);
+      })
+      .catch((err) => reject(`Unable to find project with id: ${projectId}`));
+  });
 }
 
 function getProjectsBySector(sector) {
   return new Promise((resolve, reject) => {
-    const search = sector.toLowerCase()
-    const found = projects.filter(
-      (p) => (p.sector || "").toLowerCase().includes(search)
-    )
+    Project.findAll({
+      include: [Sector],
+      where: {
+        '$Sector.sector_name$': {
+          [Sequelize.Op.iLike]: `%${sector}%`
+        }
+      }
+    })
+      .then((projects) => {
+        if (projects.length > 0) resolve(projects);
+        else reject(`Unable to find projects in sector: "${sector}"`);
+      })
+      .catch((err) => reject(`Unable to find projects in sector: "${sector}"`));
+  });
+}
 
-    if (found.length > 0) {
-      resolve(found)
-    } else {
-      reject(`Unable to find projects in sector: "${sector}"`)
-    }
+function addProject(projectData) {
+    return new Promise((resolve, reject) => {
+        Project.create(projectData)
+            .then(() => {
+                resolve()
+            })
+            .catch(err => {
+                reject(err.errors[0].message)
+            })
+    });
+}
+
+function getAllSectors() {
+  return new Promise((resolve, reject) => {
+    Sector.findAll()
+      .then(sectors => resolve(sectors))
+      .catch(err => reject("Unable to retrieve sectors"))
   })
+}
+
+function editProject(id, projectData) {
+    return new Promise((resolve, reject) => {
+        Project.update(projectData, {
+            where: { id: id }
+        })
+        .then(() => {
+            resolve()
+        })
+        .catch(err => {
+            reject(err.errors[0].message)
+        })
+    })
+}
+
+function deleteProject(id) {
+    return new Promise((resolve, reject) => {
+        Project.destroy({
+            where: { id: id }
+        })
+            .then(() => {
+                resolve()
+            })
+            .catch(err => {
+                reject(err.errors[0].message)
+            })
+    });
 }
 
 module.exports = { 
   Initialize, 
   getAllProjects, 
   getProjectById, 
-  getProjectsBySector 
+  getProjectsBySector,
+  addProject,
+  getAllSectors,
+  editProject,
+  deleteProject
 }
--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const { getProjectById } = projectData
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 8080
+const clientSessions = require("client-sessions")
+require("dotenv").config()
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
@@ -21,6 +24,27 @@ app.use(express.static("public"))
 // app.get("/", (req, res) => {
 //   res.send("Assignment 1: Jimmy Yang - 117273193")
 // })
+
+app.use(
+  clientSessions({
+    cookieName: 'session', // this is the object name that will be added to 'req'
+    secret: process.env.SESSIONSECRET, // this should be a long un-guessable string.
+    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next()
+})
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login")
+  }
+  next()
+}
 
 app.get("/", (req, res) => {
   res.render("home")
@@ -66,23 +90,78 @@ app.get("/solutions/projects/", (req, res) => {
   }
 })
 
-// app.get("/solutions/projects", (req, res) => {
-//   projectData.getAllProjects()
-//     .then(projects => res.json(projects))
-//     .catch(err => res.status(500).send(err))
-// })
+app.get("/solutions/addProject", ensureLogin, (req, res) => {
+        projectData.getAllSectors()
+        .then(sectors => {
+            res.render("addProject", { sectors: sectors })
+        })
+        .catch(err => {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` })
+        })
+})
 
-// app.get("/solutions/projects/id-demo", (req, res) => {
-//   projectData.getProjectById(5)
-//     .then(project => res.json(project))
-//     .catch(err => res.status(404).send(err))
-// })
+app.post("/solutions/addProject", ensureLogin, (req, res) => {
+    projectData.addProject(req.body)
+        .then(() => {
+            res.redirect("/solutions/projects")
+        })
+        .catch(err => {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` })
+        })
+})
 
-// app.get("/solutions/projects/sector-demo", (req, res) => {
-//   projectData.getProjectsBySector("ind")
-//     .then(found => res.json(found))
-//     .catch(err => res.status(404).send(err))
-// })
+app.get("/solutions/editProject/:id", ensureLogin, (req, res) => {
+    Promise.all([
+        projectData.getProjectById(req.params.id),
+        projectData.getAllSectors()
+    ])
+        .then(([project, sectors]) => {
+            res.render("editProject", { project: project, sectors: sectors })
+        })
+        .catch(err => {
+            res.status(404).render("404", { message: err })
+        })
+})
+
+app.post("/solutions/editProject", ensureLogin, (req, res) => {
+    projectData.editProject(req.body.id, req.body)
+        .then(() => {
+            res.redirect("/solutions/projects")
+        })
+        .catch(err => {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` })
+        })
+})
+
+app.get("/solutions/deleteProject/:id", ensureLogin, (req, res) => {
+    projectData.deleteProject(req.params.id)
+        .then(() => {
+            res.redirect("/solutions/projects")
+        })
+        .catch(err => {
+            res.render("500", { message: `I'm sorry, but we have encountered the following error: ${err}` })
+        })
+})
+
+app.get("/login", (req, res) => {
+  res.render("login", { errorMessage: null, userName: "" })
+})
+
+app.post("/login", (req, res) => {
+  const { userName, password } = req.body
+
+  if (userName === process.env.ADMINUSER && password === process.env.ADMINPASSWORD) {
+    req.session.user = { userName: userName };
+    res.redirect("/solutions/projects")
+  } else {
+    res.render("login", { errorMessage: "Invalid username or password", userName: userName })
+  }
+})
+
+app.get("/logout", (req, res) => {
+  req.session.reset()
+  res.redirect("/")
+})
 
 app.use((req, res) => {
   res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for" })
